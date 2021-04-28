a3ab7fc0a5fcaaf5a2b0c531ac460c90f2c74cc4
Gregory Casamento <greg.casamento@gmail.com>
2021-04-28 15:06:14 -0400

    Initial Commit

---
+++ CustomClassTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CustomClassTest
+//
+//  Created by Gregory Casamento on 4/28/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
