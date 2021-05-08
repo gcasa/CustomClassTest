f11075e621141dfbfb7d6d87b9d681a1571bb232
Gregory Casamento <greg.casamento@gmail.com>
2021-05-07 20:50:36 -0400

    Add custom class for button

---
+++ CustomClassTest/MyButton.h
@@ -0,0 +1,16 @@
+//
+//  MyButton.h
+//  CustomClassTest
+//
+//  Created by Gregory Casamento on 5/7/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyButton : NSButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomClassTest/MyButton.m
@@ -0,0 +1,18 @@
+//
+//  MyButton.m
+//  CustomClassTest
+//
+//  Created by Gregory Casamento on 5/7/21.
+//
+
+#import "MyButton.h"
+
+@implementation MyButton
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
